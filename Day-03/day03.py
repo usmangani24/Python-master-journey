a985715a570f8a642c71773a197e0ba924d842fb
usmangani24 <usmanhamumi@gmail.com>
2026-08-03 11:59:52 -0700

    Add personal information script in day03.py

---
+++ Day-03/day03.py
@@ -0,0 +1,13 @@
+#  My Information
+
+name = "Usmangani Hamumi"
+age = 18
+Dream = "Cars"
+Python_marster_journer = "Python Master Journey"
+AI = "AI Engineer"
+
+print(name)
+print(age)
+print(Dream)
+print(Python_marster_journer)
+print(AI)
