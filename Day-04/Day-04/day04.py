51cf51821828670c6c4f730ab7c2c885102a1a0d
usmangani24 <usmanhamumi@gmail.com>
2026-08-03 22:29:58 -0700

    Add day04.py with personal information and types

---
+++ Day-04/Day-04/day04.py
@@ -0,0 +1,14 @@
+name = "Usmangani"
+age = 18
+height = 5.9
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
