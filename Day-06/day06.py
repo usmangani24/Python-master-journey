30d4929fb8f11962c314521938a010ad2d101d71
usmangani24 <usmanhamumi@gmail.com>
2026-08-05 09:14:15 -0700

    Add user input and display details in day06.py

---
+++ Day-06/day06.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("Name:", name)
+print("Age after 5 years:", age + 5)
+print("Height:", height)
