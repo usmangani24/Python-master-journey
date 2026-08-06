1c8d9c09299ec0028dde825808be32d84d042582
usmangani24 <usmanhamumi@gmail.com>
2026-08-06 11:36:12 -0700

    Add Day 07 - Python Strings

---
+++ Day-07/day07.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Power:", num1 ** num2)
